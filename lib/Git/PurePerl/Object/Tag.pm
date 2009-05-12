aa84cd55f2a8286eee4e46fdd79d3b9be390f651
Chris Reinhardt <chris@triv.org>
2009-05-12 03:01:55 -0400

    More work on Tag objects.  Make the tagger an actor, spliting out the time to tagged_time.

--- lib/Git/PurePerl/Object/Tag.pm
+++ lib/Git/PurePerl/Object/Tag.pm
@@ -8,7 +8,8 @@ has 'kind' =>
     ( is => 'ro', isa => 'ObjectKind', required => 1, default => 'tag' );
 has 'object'  => ( is => 'rw', isa => 'Str', required => 0 );
 has 'tag'     => ( is => 'rw', isa => 'Str', required => 0 );
-has 'tagger'  => ( is => 'rw', isa => 'Str', required => 0 );
+has 'tagger'  => ( is => 'rw', isa => 'Git::PurePerl::Actor', required => 0 );
+has 'tagged_time' => ( is => 'rw', isa => 'DateTime', required => 0 );
 has 'comment' => ( is => 'rw', isa => 'Str', required => 0 );
 has 'object_kind' =>
     ( is => 'rw', isa => 'ObjectKind', required => 0);
@@ -24,9 +25,20 @@ sub BUILD {
     while ( my $line = shift @lines ) {
         last unless $line;
         my ( $key, $value ) = split ' ', $line, 2;
-        my $method = $method_map{$key} || $key;
-
-        $self->$method($value);
+        
+        if ($key eq 'tagger') {
+        	my @data = split ' ', $value;
+        	my ($email, $epoch, $tz) = splice(@data, -3);
+        	my $name = join(' ', @data);
+        	my $actor = 
+        		Git::PurePerl::Actor->new( name => $name, email => $email );
+        	$self->tagger($actor);
+            my $dt= DateTime->from_epoch( epoch => $epoch, time_zone => $tz );
+	        $self->tagged_time($dt);
+	    } else {
+			my $method = $method_map{$key} || $key;
+	        $self->$method($value);
+	    }
     }
     $self->comment( join "\n", @lines );
 }
