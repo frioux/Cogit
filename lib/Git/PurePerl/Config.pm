6975530b6faa2d9032139f3f2e214bc68a93fd42
Tomas Doran <bobtfish@bobtfish.net>
2010-01-11 19:47:16 +0000

    Leak not

--- lib/Git/PurePerl/Config.pm
+++ lib/Git/PurePerl/Config.pm
@@ -6,7 +6,7 @@ use Moose::Util::TypeConstraints;
 extends 'Config::GitLike';
 
 has '+confname' => ( default => "gitconfig" );
-has 'git'       => ( is => 'ro', isa => 'Git::PurePerl', required => 1 );
+has 'git'       => ( is => 'ro', isa => 'Git::PurePerl', required => 1, weak_ref => 1 );
 
 override dir_file => sub {
     my $self = shift;
